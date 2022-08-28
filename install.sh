d6b2acc06f5f7a79620b9601ac1a79c8ef3fa653
wynn <209275660@qq.com>
2022-08-28 12:15:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
-wget https://github.com/fatedier/frp/releases/download/v0.39.0/frp_0.39.0_linux_amd64.tar.gz
-tar -zxvf frp_0.39.0_linux_amd64.tar.gz
-rm frp_0.39.0_linux_amd64.tar.gz
-cd frp_0.39.0_linux_amd64
-
 sudo mkdir -p /etc/frp
-sudo cp frpc.ini /etc/frp
-sudo cp frpc /usr/bin
-nano /usr/lib/systemd/system/frpc.service
-sudo systemctl enable frpc
-sudo systemctl start frpc
+wget -P /usr/bin https://github.com/wynn-dev/frp-deploy/raw/main/bin/frpc
+wget -P /etc/frp https://raw.githubusercontent.com/wynn-dev/frp-deploy/main/config/frpc.ini
+wget -P /usr/lib/systemd/system https://raw.githubusercontent.com/wynn-dev/frp-deploy/main/config/frpc.service
